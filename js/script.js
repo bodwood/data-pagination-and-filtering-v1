46e673a3e843bc594e4f613df6c43c66fc65937f
bodwood <bodiewood@hotmail.com>
2022-07-24 16:20:47 -0700

    Created showPage function
    Review

--- js/script.js
+++ js/script.js
@@ -3,28 +3,57 @@ Treehouse Techdegree:
 FSJS Project 2 - Data Pagination and Filtering
 */
 
-
+/*
+* Displays a page of nine students
+* @param list - represents student data
+* @param page - represents page number
+*/
+function showPage(list, page){
+  let startIndex = (page * 9) - 9;
+  let endIndex = (page * 9);
+  let studentList = document.querySelector('.student-list');
+  studentList.innerHTML = '';
+  for(let i = 0; i < list.length; i++){
+    if(i >= startIndex && i < endIndex){
+      let studentItem = `<li class="student-item cf">
+                          <div class="student-details">
+                            <img class="avatar" src="${list[i].picture.large}" alt="Profile Picture">
+                            <h3>${list[i].name.first} ${list[i].name.last}</h3>
+                            <span class="email">${list[i].email}</span>
+                          </div>
+                          <div class="joined-details">
+                            <span class="date">${list[i].registered.date}</span>
+                          </div>
+                        </li>`;
+                        studentList.insertAdjacentHTML('beforeend', studentItem);
+    }
+  }
+}
 
 /*
-For assistance:
-   Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
-   Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
+* Creates and displays pagination buttons
+* @param list - array of student data for page
 */
- 
+function addPagination(list){
+  let numOfPages = Math.ceil(list.length/9);
+  let linkList = document.querySelector('.link-list');
+  linkList.innerHTML = '';
+  for(let i = 1; i <= numOfPages; i++){
+    let button = `<li>
+                    <button type="button">${i}</button>
+                  </li>`;
+    linkList.insertAdjacentHTML('beforeend', button);
+  }
+  document.querySelector('button').className = 'active';
+  linkList.addEventListener('click', (e) => {
+    if(e.target.tagName === 'BUTTON'){
+      document.querySelector('.active').className = '';
+      e.target.className = 'active';
+      showPage(list, e.target.textContent);
+    }
+  });
+}
 
-
-/*
-Create the `showPage` function
-This function will create and insert/append the elements needed to display a "page" of nine students
-*/
-
-
-
-/*
-Create the `addPagination` function
-This function will create and insert/append the elements needed for the pagination buttons
-*/
-
-
-
-// Call functions
+//call functions to display data
+showPage(data, 1);
+addPagination(data);
